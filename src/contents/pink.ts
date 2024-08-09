e86d4e8af9da5291853ee995e34a30bcaa0bcf46
Gabe Wyatt <gabe@gabewyatt.com>
2024-08-09 03:16:16 -0700

    api: update manifest URLs

--- src/contents/pink.ts
+++
@@ -1,8 +0,0 @@
-import { DEFAULT_PLASMO_CONFIG } from '@/constants';
-import type { PlasmoCSConfig } from 'plasmo';
-
-export const config: PlasmoCSConfig = DEFAULT_PLASMO_CONFIG;
-
-window.addEventListener('load', () => {
-  // document.body.style.background = 'pink';
-});
